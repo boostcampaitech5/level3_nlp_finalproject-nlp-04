7235c0ecf89c1c48f45fef8944275031cfdbcf23
Yungi <dbsrlskfdk@naver.com>
2023-07-10 10:07:28 +0900

    [Feature, Fix][Data] Add DB Insert Code, Fix Bug - Add Company name column - Add DB Insert Code - Fix Inter Phrase Space Bug - Fix Remove Duplicated Data Bug - Fix Change Full link Bug fix #13

--- utils/crawling_utils.py
+++ utils/crawling_utils.py
@@ -1,11 +1,14 @@
 import requests
 import json
+import os
 import re
 import pandas as pd
-import os
+import pathlib
 import time
 from selectolax.parser import HTMLParser, Node
+from supabase import create_client, Client
 from tqdm.notebook import tqdm, trange
+from datetime import datetime
 
 
 # TODO: stock_code list로 들어왔을 때 여러개 한번에 처리해서, 종목별로 반환 가능하도록 구현
@@ -22,15 +25,24 @@ def crawl_finance_news(stock_code: str = None, delay: float = 0.0) -> pd.DataFra
 	"""
 	assert stock_code is not None, "종목코드를 입력해주세요"
 
+	stock_detail = pd.read_csv(pathlib.Path.cwd().joinpath("Data", "Stock", "stock_detail.csv"), encoding="cp949")
+	stock_detail = stock_detail.loc[:, ["종목코드", "종목명", "시장구분", "업종코드", "업종명"]]
+	stock_detail["종목코드"] = stock_detail["종목코드"].apply(lambda x: str(x).zfill(6))
+	company = stock_detail[stock_detail['종목코드'] == stock_code]['종목명'].tolist()[0]
+
 	# 뉴스 페이지의 끝을 찾기 위해 크롤링 수행
 	page_max_url = f"https://finance.naver.com/item/news_news.naver?code={stock_code}"
 	page_max_html = requests.get(page_max_url).text
 	page_max_tree = HTMLParser(page_max_html)
 	nodes = page_max_tree.css("body > div > table.Nnavi > tbody > tr > td.pgRR > a")
 
-	PAGE_MAX = int(re.findall(r"(?<=page=)\d+", nodes[-1].attributes['href'])[0])
+	try:
+		PAGE_MAX = int(re.findall(r"(?<=page=)\d+", nodes[-1].attributes['href'])[0])
+	except IndexError:
+		print("기사가 존재하지 않습니다.")
+		return
 
-	content_df = pd.DataFrame([], columns=['title', 'link', "writer", 'date', 'content'])
+	content_df = pd.DataFrame([], columns=['company', 'title', 'link', "writer", 'date', 'content'])
 
 	# 전체 페이지 순환
 	for i in trange(1, PAGE_MAX + 1):
@@ -50,6 +62,7 @@ def crawl_finance_news(stock_code: str = None, delay: float = 0.0) -> pd.DataFra
 			content = []
 			tds = node.css("td")
 
+			content.append(company)
 			# 연관기사로 묶여있는 리스트가 비어있기 때문에, 아닌 것들만 기사 추출
 			if len(tds) != 0:
 				# title -> link -> writer -> date 순으로 추출
@@ -59,7 +72,7 @@ def crawl_finance_news(stock_code: str = None, delay: float = 0.0) -> pd.DataFra
 						content.append(td.css("a")[0].attributes["href"])
 
 				# 본문 추출을 위해 위에서 link를 이용해 html을 다시 요청
-				content_html = requests.get(f"https://finance.naver.com/{content[1]}").text
+				content_html = requests.get(f"https://finance.naver.com/{content[2]}").text
 				content_tree = HTMLParser(content_html)
 
 				# 본문이 없는 기사(삭제된 기사)가 종종 존재 -> 예외처리
@@ -73,24 +86,33 @@ def crawl_finance_news(stock_code: str = None, delay: float = 0.0) -> pd.DataFra
 				# 본문 추출
 				# 기사본문에 해당하는 부분만 뽑기 위해 <div class="link_news"> 이전까지만 추출
 				try:
-					body = HTMLParser(res[:re.search(r'(<div class="link_news">)', res).start()]).text(strip=True)
+					body = HTMLParser(res[:re.search(r'(<div class="link_news">)', res).start()].replace("<br>", " ")).text(strip=True) # <br> -> " "로 변경
 				except AttributeError:
-					body = HTMLParser(res).text(strip=True)
+					body = HTMLParser(res.replace("<br>", " ")).text(strip=True)
 
 				# 기사본문 내용 추가
 				content.append(body)
-				content = pd.DataFrame([content], columns=['title', 'link', "writer", 'date', 'content'])
+				content = pd.DataFrame([content], columns=['company', 'title', 'link', "writer", 'date', 'content'])
+				# full link로 수정
+				content['link'] = content['link'].apply(lambda x: "https://finance.naver.com" + x)
 				# 전체 기사에 추가
 				content_df = pd.concat([content_df, content], ignore_index=True)
 
+				insert_into_DB(content)
 			# Ban되는걸 조심하도록 delay 추가
 			time.sleep(delay)
 
-		# content 기준으로 중복 제거
-		content_df = content_df.drop_duplicates(['content'], keep='first').reset_index(drop=True)
-		# full link로 수정
-		content_df['link'] = content_df['link'].apply(lambda x: "https://finance.naver.com" + x)
-		# csv로 저장
-		content_df.to_csv(os.path.join(os.getcwd(), "Data", "News", f"{stock_code}.csv"), encoding="utf-8", index=False)
+	# content 기준으로 중복 제거
+	content_df = content_df.drop_duplicates(['content'], keep='first').reset_index(drop=True)
+
+	# csv로 저장
+	content_df.to_csv(pathlib.Path.cwd().joinpath("Data", "News", f"{stock_code}.csv"), encoding="utf-8", index=False)
 
 	return content_df
+
+def insert_into_DB(content_df: pd.DataFrame):
+	url = ""
+	key = ""
+	supabase = create_client(url, key)
+
+	_, _ = supabase.table("news").insert(content_df.iloc[0, :].to_dict()).execute()
