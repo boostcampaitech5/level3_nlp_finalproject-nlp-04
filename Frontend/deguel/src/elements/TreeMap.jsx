90022ab5a358a2468618576511b2c3910ac27037
Minseok Kwak <kwak@minseok.me>
2023-07-28 06:12:49 +0900

    [Feature] Add DB time prior
    	modified:   Frontend/deguel/src/elements/TreeMap.jsx

--- Frontend/deguel/src/elements/TreeMap.jsx
+++ Frontend/deguel/src/elements/TreeMap.jsx
@@ -80,9 +80,9 @@ export default function TreeMap(props) {
 
     // Supabase에서 데이터 가져오기. 
     async function getInformations() {
-		// let { data } = await supabase.from("keywords").select("*").order('create_time', { ascending: false }).limit(1);
-        // data = await supabase.from("keywords").select("*").eq("create_time", data[0].create_time).order('create_time', { ascending: false });
-		// data = data.data
+		let { data } = await supabase.from("keywords").select("*").order('create_time', { ascending: false }).limit(1);
+        data = await supabase.from("keywords").select("*").eq("create_time", data[0].create_time).order('create_time', { ascending: false });
+		data = data.data
 		
 		// const { data } = await supabase.from("keywords").select("*").order('create_time', { ascending: false });
 		let neg = 0, pos = 0
