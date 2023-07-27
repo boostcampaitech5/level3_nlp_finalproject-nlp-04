710ba730e01ec32d41c8ea9a47b9ace47bcbe6bf
lig96 <leeingyun96@gmail.com>
2023-07-27 12:39:10 +0000

    [Feature] Add backend batch-inference
    - train 양식과 동일하게 인퍼런스 데이터 양식을 바꿉니다.
    - 백엔드, FastAPI에서 인퍼런스를 배치로 할 수 있게 합니다.
    - style을 수정합니다.

--- Backend/Sentimental/main.py
+++ Backend/Sentimental/main.py
@@ -14,46 +14,73 @@ app = FastAPI()
 MODEL_PATH = "/opt/ml/input/model-roberta_large-sota_trainer"
 tokenizer = AutoTokenizer.from_pretrained("klue/roberta-large")
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = model.to(device)
 
 @app.get("/")
 def hello_world():
     return {"hello": "world"}
 
 
-def predict_sentiment(text):
-    model.eval()
-    with torch.no_grad() :
-        temp = tokenizer(
-            text,
-            return_tensors='pt',
-            padding=True,
-            truncation=True,
-            ##
-            max_length=100,
-            # stride=stride,
-            # return_overflowing_tokens=True,
-            return_offsets_mapping=False
-            )
-        
+def extract_sentences_token(input_dict, pad_token_id):
+    '''
+    512 토큰 초과라면 input_ids, token_type_ids, attention_maks를
+    앞 128, 뒤 384으로 분리합니다.
+    이하라면 뒤는 pad 토큰으로 채웁니다.
 
-        predicted_label = model(input_ids=temp['input_ids'],
-                                token_type_ids=temp['token_type_ids'])
-    
-    print(predicted_label)
-    
-    results = []
-    results = torch.nn.Softmax(dim=-1)(predicted_label.logits)
+    사용 방법:
+      train_encoding = tokenizer(text)
+      train_encoding = extract_sentences_token(train_encoding, tokenizer.pad_token_id)
+    '''
+    new = {}
+    batch_size = len(input_dict['input_ids'])
+    new['input_ids'] = torch.ones(batch_size, 512, dtype=int)
+    new['token_type_ids'] = torch.ones(batch_size, 512, dtype=int)
+    new['attention_mask'] = torch.ones(batch_size, 512, dtype=int)
+    # batch_size, 512
+    for i in range(batch_size):
+        a = input_dict['input_ids'][i]
+        a = a[a != pad_token_id]
+        length = len(a)
+        if length > 512:
+            left, right = 1, 3
+            a = torch.cat((a[:128*left], a[-128*right:]), dim=0)
+            new['input_ids'][i] = a
+            new['token_type_ids'][i] = input_dict['token_type_ids'][i][:512]
+            new['attention_mask'][i] = input_dict['attention_mask'][i][:512]
+        else:
+            new['input_ids'][i] = input_dict['input_ids'][i][:512]
+            new['token_type_ids'][i] = input_dict['token_type_ids'][i][:512]
+            new['attention_mask'][i] = input_dict['attention_mask'][i][:512]
+    return new
 
-
+def predict_sentiment(text: List[str]) -> List(str):
     answer = []
-    print(results)
-    for result in results :
-        if result[0]>=result[1] :
-            answer.append("부정")
-        
-        else :
-            answer.append("긍정")
-    
+    model.eval()
+
+    loader = torch.utils.data.DataLoader(dataset=text, batch_size=32, shuffle=False)
+    with torch.no_grad() :
+        for batch_text in loader:
+            temp = tokenizer(
+                batch_text,
+                return_tensors='pt',
+                padding=True,
+                truncation=True,
+                ##
+                max_length=3000, # 충분히 커서 모두 토큰화할 길이
+                )
+            temp = extract_sentences_token(temp, tokenizer.pad_token_id)
+            if torch.cuda.is_available():
+                temp = {key: value.to(device) for key, value in temp.items()}
+            predicted_label = model(**temp)
+
+            results = []
+            results = torch.nn.Softmax(dim=-1)(predicted_label.logits)
+            for result in results :
+                if result[0]>=result[1] :
+                    answer.append("부정")
+                else :
+                    answer.append("긍정")
     return answer
     
 class FinanaceSentiment(BaseModel):
