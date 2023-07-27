a1282628b7db8a3473eb8af03394316a637d81df
lig96 <leeingyun96@gmail.com>
2023-07-27 14:01:01 +0000

    [Feature] Add inference_from_saving.py
    - 커밋 0c6ef1은 evaluate 기능입니다.
    - 지금 파일이 inference 기능입니다.

---
+++ Sentimental/inference_from_saving.py
@@ -0,0 +1,41 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from utils.utils import extract_sentences_token
+
+MODEL_PATH = write_your_model_path
+tokenizer = AutoTokenizer.from_pretrained("klue/roberta-large")
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = model.to(device)
+
+arr = ["news corpus1",
+       "news corpus2"]*128
+answer = []
+
+# todo
+# 기업의 이름을 news corpus 앞에 넣는 작업이 필요합니다.
+
+model.eval()
+with torch.no_grad() :
+    loader = torch.utils.data.DataLoader(dataset=arr, batch_size=32, shuffle=False)
+    for batch_text in loader:
+        temp = tokenizer(
+            batch_text,
+            return_tensors='pt',
+            padding='max_length',
+            truncation=True,
+            max_length=3000
+            )
+        temp = extract_sentences_token(temp, tokenizer.pad_token_id)
+        if torch.cuda.is_available():
+            temp = {key: value.to(device) for key, value in temp.items()}
+        predicted_label = model(**temp)
+        print(predicted_label)
+
+        results = torch.nn.Softmax(dim=-1)(predicted_label.logits)    
+        for result in results :
+            if result[0]>=result[1] :
+                answer.append("부정")
+            else :
+                answer.append("긍정")
+print(answer)
