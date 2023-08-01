386535538931e6aa7bfa15ba8d9de70409437ee2
Minseok Kwak <kwak@minseok.me>
2023-08-01 10:15:12 +0900

    [Refectory] Rename service name
    	modified:   Frontend/deguel/index.html
    	modified:   Frontend/deguel/src/App.jsx

--- Frontend/deguel/src/App.jsx
+++ Frontend/deguel/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
 
 	return (
 		<main className="px-12 py-12 2xl:mx-32">
-			<Metric>Deguel</Metric>
+			<Metric>뉴키주</Metric>
 			<Title>현재, 언론사에서 가장 많이 다루고 있는 키워드 입니다. </Title>
 
 			<TabGroup className="mt-6">
